cd6e3e6189ccba17833cd5689519fae3cc0fbad3
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-03-21 09:42:27 +0800

    合并俩个数组

--- array/array.go
+++ array/array.go
@@ -9,7 +9,35 @@ func main() {
 	//result := findNumbers(nums)
 	//fmt.Println(result)
 	//sortedSquares(nums)
-	duplicateZeros(nums)
+	//duplicateZeros(nums)
+	merge([]int{4, 5, 6, 0, 0, 0}, 3, []int{1, 2, 3}, 3)
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	capacityIndex := len(nums1)
+	for m > 0 && n > 0 {
+		if nums1[m-1] > nums2[n-1] {
+			nums1[capacityIndex-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[capacityIndex-1] = nums2[n-1]
+			n--
+		}
+		capacityIndex--
+	}
+
+	for m > 0 {
+		nums1[capacityIndex-1] = nums1[m-1]
+		m--
+		capacityIndex--
+	}
+
+	for n > 0 {
+		nums1[capacityIndex-1] = nums2[n-1]
+		n--
+		capacityIndex--
+	}
 }
 
 func duplicateZeros(arr []int) {
