13b8aa8d8c92c99a41213f7cb288b11a5809ec44
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-06-21 09:04:27 +0800

    algorithm learn

--- queuestack/stack/custome/minstack.go
+++ queuestack/stack/custome/minstack.go
@@ -4,35 +4,30 @@ import "math"
 
 type MinStack struct {
 	Stack []int
-	Min   int
+	Min   []int
 }
 
 func Constructor() MinStack {
 	minStack := MinStack{
 		Stack: make([]int, 0),
-		Min:   math.MaxInt64,
+		Min:   []int{math.MaxInt64},
 	}
 	return minStack
 }
 
 func (this *MinStack) Push(val int) {
-	if val < this.Min {
-		this.Min = val
+	min := this.GetMin()
+	if val < min {
+		this.Min = append(this.Min, val)
+	} else {
+		this.Min = append(this.Min, min)
 	}
 	this.Stack = append(this.Stack, val)
 }
 
 func (this *MinStack) Pop() {
-	if this.Min == this.Stack[len(this.Stack)-1] {
-		this.Min = math.MaxInt64
-		for i := 0; i < len(this.Stack)-1; i++ {
-			if this.Stack[i] < this.Min {
-				this.Min = this.Stack[i]
-			}
-		}
-	}
 	this.Stack = this.Stack[:len(this.Stack)-1]
-
+	this.Min = this.Min[:len(this.Min)-1]
 }
 
 func (this *MinStack) Top() int {
@@ -40,5 +35,5 @@ func (this *MinStack) Top() int {
 }
 
 func (this *MinStack) GetMin() int {
-	return this.Min
+	return this.Min[len(this.Min)-1]
 }
