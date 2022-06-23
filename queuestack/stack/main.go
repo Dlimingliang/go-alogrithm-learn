da3a08468fd82a0b6557efeb64b22ef1a729901b
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-06-23 09:34:46 +0800

    algorithm learn

--- queuestack/stack/main.go
+++ queuestack/stack/main.go
@@ -1,10 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	//fmt.Println(isValid("([)]"))
-	fmt.Println(dailyTemperatures([]int{30, 60, 90}))
+	//fmt.Println(dailyTemperatures([]int{30, 60, 90}))
+	//fmt.Println(evalRPN([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}))
+	fmt.Println(numIslands([][]byte{
+		{'1', '1', '1', '1', '0'},
+		{'1', '1', '0', '1', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '0', '0', '0'},
+	}))
+}
+
+func numIslands(grid [][]byte) int {
+	result, n, m := 0, len(grid), len(grid[0])
+	for y := 0; y < n; y++ {
+		for x := 0; x < m; x++ {
+			if grid[y][x] == '1' {
+				numIslandsRecursion(grid, x, y, n, m)
+				result++
+			}
+		}
+	}
+	return result
+}
+
+func numIslandsRecursion(grid [][]byte, x, y, n, m int) {
+
+	if x < 0 || x == m || y < 0 || y == n || grid[y][x] == '0' {
+		return
+	}
+
+	grid[y][x] = '0'
+	numIslandsRecursion(grid, x+1, y, n, m)
+	numIslandsRecursion(grid, x-1, y, n, m)
+	numIslandsRecursion(grid, x, y+1, n, m)
+	numIslandsRecursion(grid, x, y-1, n, m)
+}
+
+func evalRPN(tokens []string) int {
+	stack := make([]int, 0)
+	for _, token := range tokens {
+		if token == "+" || token == "-" || token == "*" || token == "/" {
+			tempPre := stack[len(stack)-2]
+			tempNext := stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+			if token == "+" {
+				stack = append(stack, tempPre+tempNext)
+			} else if token == "-" {
+				stack = append(stack, tempPre-tempNext)
+			} else if token == "*" {
+				stack = append(stack, tempPre*tempNext)
+			} else if token == "/" {
+				stack = append(stack, tempPre/tempNext)
+			}
+		} else {
+			intToken, _ := strconv.Atoi(token)
+			stack = append(stack, intToken)
+		}
+	}
+	return stack[0]
 }
 
 func dailyTemperatures(temperatures []int) []int {
