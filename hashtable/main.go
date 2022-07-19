74f96c5086e667927cf3732cfc8c7e575ae0b0a6
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-07-19 09:59:15 +0800

    algorithm learn

--- hashtable/main.go
+++ hashtable/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"math"
 	"sort"
@@ -63,6 +64,44 @@ func main() {
 	//fmt.Println(numJewelsInStones("z", "ZZ"))
 	//fmt.Println(lengthOfLongestSubstring("asljlj"))
 	fmt.Println(fourSumCount([]int{-1, 1, 1, 1, -1}, []int{0, -1, -1, 0, 1}, []int{-1, -1, 1, -1, -1}, []int{0, 1, 0, -1, -1}))
+}
+
+func topKFrequent(nums []int, k int) []int {
+	occurrences := map[int]int{}
+	for _, num := range nums {
+		occurrences[num]++
+	}
+	h := &IHeap{}
+	heap.Init(h)
+	for key, value := range occurrences {
+		heap.Push(h, [2]int{key, value})
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	ret := make([]int, k)
+	for i := 0; i < k; i++ {
+		ret[k-i-1] = heap.Pop(h).([2]int)[0]
+	}
+	return ret
+}
+
+type IHeap [][2]int
+
+func (h IHeap) Len() int           { return len(h) }
+func (h IHeap) Less(i, j int) bool { return h[i][1] < h[j][1] }
+func (h IHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IHeap) Push(x interface{}) {
+	*h = append(*h, x.([2]int))
+}
+
+func (h *IHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
 }
 
 func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
