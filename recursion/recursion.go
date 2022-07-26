1e8ba268d9b5bd8519e4da4e560c77ea567b56fa
limingliangaiduyangyang <lml2dyy@gmail.com>
2022-07-26 08:56:55 +0800

    我的LRU提交

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -1,7 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
-	reverseString([]byte{'h', 'e', 'l', 'l', 'o'})
+	//reverseString([]byte{'h', 'e', 'l', 'l', 'o'})
+	node4 := ListNode{Val: 4}
+	//node3 := ListNode{Val: 3, Next: &node4}
+	//node2 := ListNode{Val: 2, Next: &node3}
+	//node1 := ListNode{Val: 1, Next: &node2}
+	head := swapPairs(&node4)
+	fmt.Println(head)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := head.Next
+	head.Next = swapPairs(newHead.Next)
+	newHead.Next = head
+	return newHead
 }
 
 func reverseString(s []byte) {
